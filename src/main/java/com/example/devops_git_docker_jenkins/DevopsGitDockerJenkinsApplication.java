cc985fc23f37cd9ac0d4c12e9f150905184c655b
krishnasampath23 <krishnasampath23@gmail.com>
2023-10-13 11:47:50 -0500

    first commit

---
+++ src/main/java/com/example/devops_git_docker_jenkins/DevopsGitDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_git_docker_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGitDockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGitDockerJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops_git_docker_jenkins/DevopsGitDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_git_docker_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGitDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
